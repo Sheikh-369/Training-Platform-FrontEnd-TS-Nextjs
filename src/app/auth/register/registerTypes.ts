e3254f6fde52f9d6a92bd75d2dda0b1e592e7683
Zunaid Sheikh <lamhaa786@gmail.com>
2025-07-12 11:05:48 +0545

    register

--- src/app/auth/register/registerTypes.ts
+++ src/app/auth/register/registerTypes.ts
@@ -4,10 +4,3 @@ export interface IInitialUserData{
     userPassword:string
 }
 
-echo "# FrontEnd-Training-Platform-TS-Nextjs" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/Sheikh-369/FrontEnd-Training-Platform-TS-Nextjs.git
-git push -u origin master
