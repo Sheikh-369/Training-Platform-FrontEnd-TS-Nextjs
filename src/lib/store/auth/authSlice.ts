4dfa9a89daaa2eefff7d78745f23d166bd7e5646
Zunaid Sheikh <lamhaa786@gmail.com>
2025-07-18 14:26:11 +0545

    loginThunk

--- src/lib/store/auth/authSlice.ts
+++ src/lib/store/auth/authSlice.ts
@@ -3,6 +3,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IInitialState, IUserData } from "./authSliceTypes";
 import API from "@/lib/http";
 import { AppDispatch } from "../store";
+import { IUserLoginData } from "@/app/auth/login/loginTypes";
 
 const initialState:IInitialState={
     user:{
@@ -34,10 +35,29 @@ export {setUser,setStatus}
 export function userRegrister(data:IUserData){
     return async function userRegisterThunk(dispatch:AppDispatch){
         try {
-            const response=await API.post("register",data)
+            const response=await API.post("auth/register",data)
             console.log(response)
             if(response.status===201){
-                //code to if works fine
+                //code if works fine
+                dispatch(setStatus(Status.SUCCESS))
+            }else{
+                dispatch(setStatus(Status.ERROR))
+            }
+        } catch (error) {
+            console.log(error)
+            dispatch(setStatus(Status.ERROR))
+        }
+    }
+}
+
+
+export function userLogin(data:IUserLoginData){
+    return async function userLoginThunk(dispatch:AppDispatch){
+        try {
+            const response=await API.post("auth/login",data)
+            console.log(response)
+            if(response.status===200){
+                //code if works fine
                 dispatch(setStatus(Status.SUCCESS))
             }else{
                 dispatch(setStatus(Status.ERROR))
